13c705945a2a21edbad1820817f9f717dcfc56b6
Jett <jettc505@gmail.com>
2023-06-19 12:47:33 -0600

    _

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// This project is a simple find and replace algorithm.
+// The project will read a file find a specified string of
+// characters, and replace it with a specified string.
+use std::any::type_name;
+use std::fs;
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
+
+fn find(path: &str, search: &str) /*  ->  Result<(), Error>  */
+{
+    let file = fs::read_to_string(path);
+
+    println!("The type is {}", type_of(file))
+}
+
 fn main() {
-    println!("Hello, world!");
+    find("test.txt", "The");
 }
